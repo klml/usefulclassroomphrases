d79c4470c67f66283b47dc496c318186f1c68a15
klml <m@klml.de>
2012-11-02 20:26:53 +0100

    hashspliter

--- jquery.js
+++ jquery.js
@@ -1,32 +1,35 @@
 $(document).ready(function() {
-  hashtagger(window.location.hash);
-  hashslicer(window.location.hash); 
-  $('a[href^="#"]').click(function() {     
-		hashtagger(this.hash);
-		hashslicer(this.hash);
+  hashspliter(window.location.hash);
+  
+  $('.demo a[href^="#"]').click(function() {
+        hashtagger(this.hash);
     });
+  $('a[href^="#"]').click(function() {
+        hashspliter(this.hash);
+    });
+    
 });
  
 function hashtagger (hash) {
   // poor mans tab
-	$('.hashtagger').hide();
-	$(hash).show(); 
+    $('.hashtagger > div').hide();
+    $(hash).show(); 
   // special actions
-	switch (hash) {
+    switch (hash) {
   case "#contact": // example
-  
+    alert('fork me at github/klml')
     break;
   default:
   case "#":
 
     break;
-	}
+    }
 }
-function hashslicer (hash) { // prefill "GET parameter"-like value string to inputs
-	var gets = hash.slice('1').split('&') ;
+function hashspliter (hash) { // prefill "GET parameter"-like value string to inputs
+    var gets = hash.slice('1').split('&') ;
 
-	for (var i = 0; i < gets.length; i++) {
-		var getkeyparam = gets[i].split('=')
-		$('#' + getkeyparam[0]).val( getkeyparam[1] )
-	}
+    for (var i = 0; i < gets.length; i++) {
+        var getkeyparam = gets[i].split('=')
+        $('#' + getkeyparam[0]).val( getkeyparam[1] )
+    }
 }
