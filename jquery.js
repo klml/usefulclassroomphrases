d562f03d774b966fb1626ca4cb3d940eed7e1ffa
MurphyMacManus <info@mg-webdevelopment.de>
2013-04-08 11:22:35 +0300

    Bugfixes
    changed hashsplitter if statement, fixed hashtagger exceptions, added semicolons

--- jquery.js
+++ jquery.js
@@ -22,28 +22,27 @@ function pooraccordion(animate) {
 
 function hashtagger (hash, exception) {
     //hashspliter: prefill "GET parameter"-like value string to inputs
-    var gets = hash.slice('1').split('&') ;
-    if (gets.length != 1) {
+    var gets = hash.slice('1').split('&');
+    if (gets.indexOf('=') > -1) {
         for (var i = 0; i < gets.length; i++) {
-            var getkeyparam = gets[i].split('=')
-            $('#' + getkeyparam[0]).val( getkeyparam[1] )
+            var getkeyparam = gets[i].split('=');
+            $('#' + getkeyparam[0]).val( getkeyparam[1] );
         }
-        return
-    }
-    // poor mans tab
-    jQuery('.hashtagger > div, .hashtabber').not( exception ).hide();
-    jQuery(hash).show();
-    jQuery(hash).click(); // to use with jQuery UI Accordion etc
-
-    // special actions
-    switch (hash) {
-        case "#alert": // example
-            alert('fork me at github.com/klml/usefulclassroomphrases')
-        break;
-        default:
-        case "#":
-
-        break;
-    }
-
+    } else {
+        // poor mans tab
+        jQuery('.hashtagger > div, .hashtabber').not( exception ).hide();
+        jQuery(hash).show();
+        jQuery(hash).click(); // to use with jQuery UI Accordion etc
+    
+        // special actions
+        switch (hash) {
+            case "#alert": // example
+                alert('fork me at github.com/klml/usefulclassroomphrases');
+            break;
+            default:
+            case "#":
+    
+            break;
+        }
+    }    
 }
